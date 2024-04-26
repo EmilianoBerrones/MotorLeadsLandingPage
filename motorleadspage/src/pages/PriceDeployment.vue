ae3f8238f25c479153c9cad2861e134d515a26be
NeRvCobra <A01770892@tec.mx>
2024-04-26 08:58:20 -0600

    Agregar price deployment
    se creó la pagina de despliegue de precio y se agrego la ruta.

---
+++ motorleadspage/src/pages/PriceDeployment.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h2 class="text-center">{{ pageTitle }}</h2>
+    <q-splitter
+      v-model="splitterModel"
+      style="height: 200px"
+    >
+
+      <template v-slot:before>
+        <q-tabs
+          v-model="tab"
+          vertical
+          class="text-grey-8"
+        >
+          <q-tab name="precio" icon="attach_money" label="Precio" />
+          <q-tab name="vehículo" icon="directions_car" label="Vehículo" />
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="precio">
+            <div class="text-h4 q-mb-md">Precio</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="vehículo">
+            <div class="text-h4 q-mb-md">Vehículo</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+
+    </q-splitter>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: "PriceDeployment",
+  setup () {
+    return {
+      tab: ref('precio'),
+      splitterModel: ref(20)
+    }
+  },
+  data (){
+    return {
+      pageTitle: 'Consulta el Precio de tu Vehículo',
+    };
+  }
+}
+</script>
--- motorleadspage/src/router/routes.js
+++ motorleadspage/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'options', component: () => import('pages/OptionsPage.vue')}
+      { path: 'options', component: () => import('pages/OptionsPage.vue')},
+      {path: '/price', component: () => import('pages/PriceDeployment.vue')}
     ]
   },
 
