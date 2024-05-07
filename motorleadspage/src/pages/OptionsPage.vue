8e762346a5d652af14e69d413460f4481217fe91
Emiliano Berrones <emilianoberrones@gmail.com>
2024-05-07 17:24:56 -0600

    Update OptionsPage.vue
    OptionsPage modifica el tamaño de sus componentes dependiendo del tamaño de la pantalla.

--- motorleadspage/src/pages/OptionsPage.vue
+++ motorleadspage/src/pages/OptionsPage.vue
@@ -19,18 +19,24 @@
          backgroundColor:'white',
          borderRadius:'16px',
          zIndex: 1}">
-      <div class="col-4">
-        <h5>Selecciona una marca</h5>
+      <div class="col-xl-6 col-md-8">
+        <div v-if="$q.screen.xl || $q.screen.gt.lg" class="text-h5" style="padding-top: 25px; padding-bottom: 25px">Selecciona una marca</div>
+        <div v-else class="text-h6" style="padding-top: 5px; padding-bottom: 5px">Selecciona una marca</div>
         <q-btn-dropdown class="full-width"></q-btn-dropdown>
-        <h5>Selecciona un modelo</h5>
+        <div v-if="$q.screen.xl || $q.screen.gt.lg" class="text-h5" style="padding-top: 25px; padding-bottom: 25px">Selecciona un modelo</div>
+        <div v-else class="text-h6" style="padding-top: 5px; padding-bottom: 5px">Selecciona un modelo</div>
         <q-btn-dropdown class="full-width"></q-btn-dropdown>
-        <h5>Selecciona un año</h5>
+        <div v-if="$q.screen.xl || $q.screen.gt.lg" class="text-h5" style="padding-top: 25px; padding-bottom: 25px">Selecciona un año</div>
+        <div v-else class="text-h6" style="padding-top: 5px; padding-bottom: 5px">Selecciona un año</div>
         <q-btn-dropdown class="full-width"></q-btn-dropdown>
-        <h5>Selecciona una version</h5>
+        <div v-if="$q.screen.xl || $q.screen.gt.lg" class="text-h5" style="padding-top: 25px; padding-bottom: 25px">Selecciona una version</div>
+        <div v-else class="text-h6" style="padding-top: 5px; padding-bottom: 5px">Selecciona una version</div>
         <q-btn-dropdown class="full-width"></q-btn-dropdown>
-        <h5>Agrega un kilometraje</h5>
+        <div v-if="$q.screen.xl || $q.screen.gt.lg" class="text-h5" style="padding-top: 25px; padding-bottom: 25px">Agrega un kilometraje</div>
+        <div v-else class="text-h6" style="padding-top: 5px; padding-bottom: 5px">Agrega un kilometraje</div>
         <q-input></q-input>
-        <h5>Selecciona un color</h5>
+        <div v-if="$q.screen.xl || $q.screen.gt.lg" class="text-h5" style="padding-top: 25px; padding-bottom: 25px">Selecciona un color</div>
+        <div v-else class="text-h6" style="padding-top: 5px; padding-bottom: 5px">Selecciona un color</div>
         <q-btn-dropdown class="full-width"></q-btn-dropdown>
         <h6>
           <q-btn glossy color="primary" class="full-width" size="lg" label="Buscar"></q-btn>
